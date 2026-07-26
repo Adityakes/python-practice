3f0250152994607241b270c20bdb80551524be87
Aditya Keshri <keshriaditya167@gmail.com>
2026-07-26 11:57:51 +0530

    Added arithmetic operators examples

---
+++ 02_Operators/arithmetic_operators.py
@@ -0,0 +1,21 @@
+"""
+Program: Arithmetic Operators
+Author: Aditya Keshri
+Description: Basic arithmetic operators in Python
+"""
+
+a = int(input("Enter Your 1st Number: "))
+b = int(input("Enter Your 2nd Number: "))
+
+print("a =", a)
+print("b =", b)
+
+print()
+
+print("Addition       :", a + b)
+print("Subtraction    :", a - b)
+print("Multiplication :", a * b)
+print("Division       :", a / b)
+print("Floor Division :", a // b)
+print("Modulus        :", a % b)
+print("Exponent       :", a ** b)
