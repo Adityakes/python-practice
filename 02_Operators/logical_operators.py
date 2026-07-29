a35027a0c5503ed2ec4871338c41fc76800ffff5
Aditya Keshri <keshriaditya167@gmail.com>
2026-07-29 05:52:48 +0530

    Added logical operators examples

---
+++ 02_Operators/logical_operators.py
@@ -0,0 +1,17 @@
+"""
+Program: Logical Operators
+Author: Aditya Keshri
+Description: Logical operators in Python
+"""
+
+a = True
+b = False
+
+print("a =", a)
+print("b =", b)
+print()
+
+print("a and b :", a and b)
+print("a or b  :", a or b)
+print("not a   :", not a)
+print("not b   :", not b)
