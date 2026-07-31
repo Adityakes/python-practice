ef43d456518f02337a75ebc4f4ef3e0bf53d8b45
Aditya Keshri <keshriaditya167@gmail.com>
2026-07-31 21:57:07 +0530

    Added basic if statement examples

---
+++ 03_Conditional_Statements/if_statement.py
@@ -0,0 +1,32 @@
+"""
+Program: If Statement
+Author: Aditya Keshri
+Description: Basic if statement in Python
+"""
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+
+print("Program Ended")
+
+
+# Driving License Check
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible for Driving License")
+
+
+# Pass Check
+marks = int(input("Enter your marks: "))
+
+if marks >= 40:
+    print("Pass")
+
+# Temperature Check
+temperature = int(input("Enter temperature: "))
+
+if temperature > 35:
+    print("It is Hot Today")
