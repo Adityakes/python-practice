335a330fc3f5599a810cf2abd95ae0f12da03fca
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-05 08:43:12 +0530

    Added basic for loop examples

---
+++ 04_loops/for_loop_basics.py
@@ -0,0 +1,19 @@
+"""
+Program: For Loop Basics
+Author: Aditya Keshri
+Description: Print numbers using for loop
+"""
+
+for i in range(5):
+    print(i)
+
+
+# For range(START END STEP)
+# Print Number Backward
+for i in range(10, 0, -1):
+    print(i)
+
+# Print Even Number
+for i in range(2, 21, 2):
+    print(i)
+    
