a68f8d4c6a221812572b61f8b8df08b43e45639a
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-06 12:08:44 +0530

    Added basic for loop examples

--- 04_loops/for_loop_basics.py
+++ 04_loops/for_loop_basics.py
@@ -4,16 +4,30 @@ Author: Aditya Keshri
 Description: Print numbers using for loop
 """
 
-for i in range(5):
+# Program 1: Print 1 to 10
+for i in range(1, 11):
     print(i)
 
-
-# For range(START END STEP)
-# Print Number Backward
+# Program 2: Print 10 to 1
 for i in range(10, 0, -1):
     print(i)
 
-# Print Even Number
+# Program 3: Even Numbers
 for i in range(2, 21, 2):
     print(i)
-    
+
+# Program 4: Odd Numbers
+for i in range(1, 20, 2):
+    print(i)
+
+# Program 5: Table of 5
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
+
+# Program 6: User Input Table ⭐
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
+
+
