e22e69c1b4a230372e4ce6c8f51063169d759820
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-06 12:18:01 +0530

    Added basic for nested loop examples

---
+++ 04_loops/nested_for_loop.py
@@ -0,0 +1,28 @@
+"""
+Program: Nested For Loop
+Author: Aditya Keshri
+Description: Print numbers using nested loops
+"""
+# Program 1 - Nested Loop
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(j, end=" ")
+    print()
+
+# Program 2 - Increasing Star Pattern
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+# Program 3 - Reverse Star Pattern
+for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+# Program 4 - Number Pattern
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
