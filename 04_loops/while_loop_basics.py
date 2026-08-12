55894a30376c2c1281c9c2c4c79f614f4a5215cc
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-12 18:30:30 +0530

    Added basic while loop examples

---
+++ 04_loops/while_loop_basics.py
@@ -0,0 +1,22 @@
+"""
+Program: While Loop Basics
+Author: Aditya Keshri
+Description: Basic examples of while loop
+"""
+
+# Program 1 - Print numbers from 1 to 5
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+
+
+# Program 2 - Reverse counting
+
+i = 5
+
+while i >= 1:
+    print(i)
+    i = i - 1
