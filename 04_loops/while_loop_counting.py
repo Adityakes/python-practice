d6be1576c9555af5afe8b92d038707878212d255
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-12 18:33:19 +0530

    Added basic while loop counting  examples

---
+++ 04_loops/while_loop_counting.py
@@ -0,0 +1,11 @@
+"""
+Program: While Loop Counting
+Author: Aditya Keshri
+Description: Print reverse counting using user input
+"""
+
+num = int(input("Enter a number: "))
+
+while num >= 1:
+    print(num)
+    num = num - 1
