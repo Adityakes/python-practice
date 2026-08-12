3ad477859e81be7e501430d391448bff7646f0a7
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-12 18:35:25 +0530

    Added user controlled  while loop

---
+++ 04_loops/while_user_controlled.py
@@ -0,0 +1,12 @@
+"""
+Program: User Controlled While Loop
+Author: Aditya Keshri
+Description: Continue the loop until the user enters 5
+"""
+
+num = 0
+
+while num != 5:
+    num = int(input("Enter 5 to exit: "))
+
+print("Program ended")
