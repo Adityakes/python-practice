ea5f6e83f59ddff50e2338e7c769260f13314941
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-14 16:03:53 +0530

    Added basic function examples

---
+++ 05_functions/cube_by_fun.py
@@ -0,0 +1,13 @@
+"""
+Program: Function Basics
+Author: Aditya Keshri
+Description: Basic examples of Python functions
+"""
+# Program - Cube using User Input
+
+def cube(number):
+    print("Cube of number:", number ** 3)
+
+
+num = int(input("Enter your number: "))
+cube(num)
