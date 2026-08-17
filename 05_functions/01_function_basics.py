e62b62f7f34c555b4f8469e8e3afeba0d9885ec8
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-17 12:14:13 +0530

    Added function basics

---
+++ 05_functions/01_function_basics.py
@@ -0,0 +1,24 @@
+"""
+Program: Function Basics
+Author: Aditya Keshri
+Description: Basic examples of defining and calling functions
+"""
+
+# Program 1 - Basic Function
+
+def greet():
+    print("Hello Aditya!")
+
+
+greet()
+
+
+# Program 2 - Calling a Function Multiple Times
+
+def welcome():
+    print("Welcome to Python!")
+
+
+welcome()
+welcome()
+welcome()
