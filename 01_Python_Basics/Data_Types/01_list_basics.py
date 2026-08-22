437664bf378ebb813dc4cd8fc369c1d847d2552d
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-22 23:49:31 +0530

    Added basic list practice

---
+++ 01_Python_Basics/Data_Types/01_list_basics.py
@@ -0,0 +1,16 @@
+"""
+Program: List Basics
+Author: Aditya Keshri
+Description: Basic operations with Python lists
+"""
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Fruits:", fruits)
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+fruits.append("Grapes")
+
+print("After adding a fruit:", fruits)
+print("Total fruits:", len(fruits))
