fedd4e8784487b94c03e1cd9ae20eece59d38ef6
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-22 23:55:33 +0530

    Added basic set practice

---
+++ 01_Python_Basics/Data_Types/04_set_basics.py
@@ -0,0 +1,13 @@
+"""
+Program: Set Basics
+Description: Basic operations with Python sets
+"""
+
+numbers = {10, 20, 30, 20, 40}
+
+print("Set:", numbers)
+
+numbers.add(50)
+
+print("After adding:", numbers)
+print("Total elements:", len(numbers))
