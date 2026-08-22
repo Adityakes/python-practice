f0f0f716ed6cf13b0026b82f026a0fd5b91eac89
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-22 23:52:28 +0530

    Added basic tuple practice

---
+++ 01_Python_Basics/Data_Types/02_tuple_basics.py
@@ -0,0 +1,11 @@
+"""
+Program: Tuple Basics
+Description: Basic operations with Python tuples
+"""
+
+colors = ("Red", "Green", "Blue", "Yellow")
+
+print("Colors:", colors)
+print("First color:", colors[0])
+print("Last color:", colors[-1])
+print("Total colors:", len(colors))
