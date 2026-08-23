b626e0074de9017a0cf04029af1bfd547d08b76d
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-23 23:16:45 +0530

    Added student data type practice

---
+++ 01_Python_Basics/Data_Types/05_student_data.py
@@ -0,0 +1,20 @@
+"""
+Program: Student Data
+Description: Using different Python data types together
+"""
+
+name = "Aditya"
+age = 21
+skills = ["Python", "Git", "GitHub"]
+courses = ("Python", "DSA", "DBMS")
+student = {
+    "name": name,
+    "age": age,
+    "skills": skills,
+    "courses": courses
+}
+
+print("Student Name:", student["name"])
+print("Age:", student["age"])
+print("Skills:", student["skills"])
+print("Courses:", student["courses"])
