3aab5576f70b742ebd1a6368331427be7c6b09e5
Aditya Keshri <keshriaditya167@gmail.com>
2026-08-24 13:24:52 +0530

    Added pattern practice

---
+++ 04_loops/pattern/01_star.py
@@ -0,0 +1,13 @@
+'''
+Author "Aditya Keshri"
+program : Increasing Star pattern
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+'''
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
