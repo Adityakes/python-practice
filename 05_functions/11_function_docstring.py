67f12041c11cfdda04831d1474769aa7c90536a5
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-02 23:42:48 +0530

    added function docstring  practice

---
+++ 05_functions/11_function_docstring.py
@@ -0,0 +1,13 @@
+"""
+Program: Function Docstring Practice
+Description: Practice using docstrings in Python functions
+"""
+
+def greet(name):
+    """Print a greeting message for the given name."""
+    print("Hello", name)
+
+
+greet("Aditya")
+
+print(greet.__doc__)
