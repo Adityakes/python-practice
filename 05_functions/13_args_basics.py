ff8d11681606f78d6628bec4b076a294950eaece
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-03 23:28:11 +0530

    added args basics practice

---
+++ 05_functions/13_args_basics.py
@@ -0,0 +1,14 @@
+# *args allows a function to accept multiple positional arguments
+
+def add_numbers(*args):
+    total = 0
+
+    for num in args:
+        total += num
+
+    return total
+
+
+print(add_numbers(10, 20))
+print(add_numbers(10, 20, 30))
+print(add_numbers(1, 2, 3, 4, 5))
