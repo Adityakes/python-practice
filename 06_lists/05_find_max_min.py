d667807a5c46c36172a3cd1833458101305b0ea5
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-06 23:27:15 +0530

    Add maximum and minimum list program

---
+++ 06_lists/05_find_max_min.py
@@ -0,0 +1,9 @@
+# Create a list
+numbers = [25, 10, 45, 5, 30, 60]
+
+# Find maximum and minimum
+maximum = max(numbers)
+minimum = min(numbers)
+
+print("Maximum:", maximum)
+print("Minimum:", minimum)
