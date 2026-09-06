3965fce0e719a232b62cc8dacca5987b48e0b6e8
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-06 23:21:51 +0530

    Add elements to list program

---
+++ 06_lists/02_add_elements.py
@@ -0,0 +1,13 @@
+# Create a list
+fruits = ["Apple", "Banana"]
+
+# Add one element
+fruits.append("Mango")
+
+# Add an element at a specific position
+fruits.insert(1, "Orange")
+
+# Add multiple elements
+fruits.extend(["Grapes", "Pineapple"])
+
+print(fruits)
