fb64024e76c5c149d46784d781cf44469968e2e0
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-06 23:23:35 +0530

    Add elements to list removal program

---
+++ 06_lists/03_remove_elements.py
@@ -0,0 +1,13 @@
+# Create a list
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+# Remove an element by value
+fruits.remove("Mango")
+
+# Remove the last element
+fruits.pop()
+
+# Remove an element using index
+del fruits[0]
+
+print(fruits)
