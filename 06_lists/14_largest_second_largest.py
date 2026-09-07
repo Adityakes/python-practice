d49da4a00a3258ffdd6e456d2efbef170d6fabed
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-08 00:22:30 +0530

    Add largest and second largest program

---
+++ 06_lists/14_largest_second_largest.py
@@ -0,0 +1,16 @@
+numbers = [25, 10, 45, 67, 34, 89, 52]
+
+largest = float("-inf")
+second_largest = float("-inf")
+
+for number in numbers:
+    if number > largest:
+        second_largest = largest
+        largest = number
+
+    elif number > second_largest and number != largest:
+        second_largest = number
+
+print("List:", numbers)
+print("Largest:", largest)
+print("Second largest:", second_largest)
