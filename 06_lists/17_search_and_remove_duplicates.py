61ff7391b694f49f5962d8334c1279188b31154d
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-09 19:31:47 +0530

    added list search and even odd practice

--- 06_lists/17_search_and_remove_duplicates.py
+++ 06_lists/17_search_and_remove_duplicates.py
@@ -1,7 +1,23 @@
 '''
 Program: search and remove duplicate Numbers
 Author: "Aditya Keshri"
-Description: Finds The Average and sum of the numbers
+Description: Finds And Remove The Duplicate Numbers
 '''
 
 
+numbers = [10, 20, 10, 30, 20, 40]
+
+# Search
+search_element = 30
+
+if search_element in numbers:
+    print(search_element, "is present in the list")
+else:
+    print(search_element, "is not present in the list")
+
+
+# Remove duplicates
+unique_numbers = list(set(numbers))
+
+print("Original List:", numbers)
+print("List without duplicates:", unique_numbers)
