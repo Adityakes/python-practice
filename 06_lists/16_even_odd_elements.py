4a4636f7b601fab3de42691de3f9ed733117c152
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-09 19:22:06 +0530

    added list practice programs

---
+++ 06_lists/16_even_odd_elements.py
@@ -0,0 +1,20 @@
+'''
+Program: sum_and_average
+Author: "Aditya Keshri"
+Description: Finds The Odd And Even of the numbers
+'''
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+even_numbers = []
+odd_numbers = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+    else:
+        odd_numbers.append(num)
+
+print("Numbers:", numbers)
+print("Even Numbers:", even_numbers)
+print("Odd Numbers:", odd_numbers)
