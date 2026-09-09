31d5e68e53aec70a764487487d9782a6276520db
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-09 19:20:12 +0530

    added list practice programs

---
+++ 06_lists/15_sum_and_average.py
@@ -0,0 +1,14 @@
+'''
+Program: sum_and_average
+Author: "Aditya Keshri"
+Description: Finds The Average and sum of the numbers
+'''
+
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+average = total / len(numbers)
+
+print("Numbers:", numbers)
+print("Sum:", total)
+print("Average:", average)
