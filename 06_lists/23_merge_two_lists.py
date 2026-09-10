94cc24a49f847193dc128e505e8c0c99fa2922f9
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-11 00:50:48 +0530

    added list practice

---
+++ 06_lists/23_merge_two_lists.py
@@ -0,0 +1,14 @@
+"""
+Program: Merge Two Lists
+Author: "Aditya Keshri"
+Description: Combines two lists into one list.
+"""
+
+list1 = [10, 20, 30]
+list2 = [40, 50, 60]
+
+merged_list = list1 + list2
+
+print("List 1:", list1)
+print("List 2:", list2)
+print("Merged List:", merged_list)
