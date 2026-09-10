e14cb0859375c2f3bee112835657b51cfd118ede
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-10 16:41:03 +0530

    added second smallest element practice

---
+++ 06_lists/20_second_smallest.py
@@ -0,0 +1,14 @@
+"""
+Program: Find the Second Smallest Element
+Author: "Aditya Keshri"
+Description: Finds the second smallest element in a list.
+"""
+
+numbers = [25, 10, 40, 5, 20]
+
+numbers.sort()
+
+second_smallest = numbers[1]
+
+print("Sorted List:", numbers)
+print("Second Smallest Element:", second_smallest)
