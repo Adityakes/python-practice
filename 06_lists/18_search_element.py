c75c03efb65789ad3575934ae7efdf6d10db39a2
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-10 16:36:12 +0530

    added list search  practice

---
+++ 06_lists/18_search_element.py
@@ -0,0 +1,14 @@
+"""
+Program: Search an Element in a List
+Author: "Aditya Keshri"
+Description: Checks whether a given element is present in the list.
+"""
+
+numbers = [10, 20, 30, 40, 50]
+
+search_element = 30
+
+if search_element in numbers:
+    print(search_element, "is present in the list")
+else:
+    print(search_element, "is not present in the list")
