aff65d35beb3aedfc29ad8e34cae0c62503b24c2
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-16 07:23:58 +0530

    added Moves all zeroes to the end of the list while maintaining the order of other elements.  dsa practice

---
+++ 06_lists/DSA_list/09_move_zeroes.py
@@ -0,0 +1,21 @@
+"""
+Program: Move Zeroes
+Author: Aditya Keshri
+Description: Moves all zeroes to the end of the list while maintaining the order of other elements.
+"""
+
+numbers = [0, 1, 0, 3, 12]
+
+result = []
+
+for num in numbers:
+    if num != 0:
+        result.append(num)
+
+zero_count = numbers.count(0)
+
+for i in range(zero_count):
+    result.append(0)
+
+print("Original List:", numbers)
+print("After Moving Zeroes:", result)
