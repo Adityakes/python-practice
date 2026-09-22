a01f7df99573a1662f69c904f3765eeb7133777f
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-23 02:07:01 +0530

    added tuple creation and access practice

---
+++ 07_tuples/01_create_and_access_tuple.py
@@ -0,0 +1,12 @@
+"""
+Program: Create and Access Tuple
+Author: Aditya Keshri
+Description: Creates a tuple and accesses its elements using indexes.
+"""
+
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+print("First Element:", numbers[0])
+print("Third Element:", numbers[2])
+print("Last Element:", numbers[-1])
