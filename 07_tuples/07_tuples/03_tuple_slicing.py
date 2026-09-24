073a4b5c3789434039b7fd72411da8ad80240f8b
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-24 06:44:30 +0530

    added tuple slicing  practice

---
+++ 07_tuples/07_tuples/03_tuple_slicing.py
@@ -0,0 +1,13 @@
+"""
+Program: Tuple Slicing
+Author: Aditya Keshri
+Description: Demonstrates slicing operations on a tuple.
+"""
+
+numbers = (10, 20, 30, 40, 50, 60)
+
+print("Tuple:", numbers)
+print("First Three:", numbers[:3])
+print("Middle Elements:", numbers[2:5])
+print("Last Three:", numbers[-3:])
+print("Reverse:", numbers[::-1])
