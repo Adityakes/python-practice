a7748ed4de382a132f0cb0a706c91fb4b06e1520
Aditya Keshri <keshriaditya167@gmail.com>
2026-09-24 06:53:07 +0530

    fixed tuple files and added unpacking practice

---
+++ 07_tuples/04_tuple_unpacking.py
@@ -0,0 +1,14 @@
+"""
+Program: Tuple Unpacking
+Author: Aditya Keshri
+Description: Demonstrates how tuple elements can be assigned to multiple variables.
+"""
+
+numbers = (10, 20, 30)
+
+a, b, c = numbers
+
+print("Tuple:", numbers)
+print("A:", a)
+print("B:", b)
+print("C:", c)
